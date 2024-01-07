ad14633e639ef27ec6f70a645d861636e78f5359
KaterynaRadchenko <82673991+Katia-Radchenko@users.noreply.github.com>
2024-01-07 22:32:05 +0200

    update statList

--- src/components/Statistics/StatList/StatList.jsx
+++ src/components/Statistics/StatList/StatList.jsx
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import StatsListItem from './StatsListItem/StatsListItem';
 import {StatsList} from './StatList.styled';
+import Statistics from '../Statistics';
 
 export default function StatList({ stats = [] }) {
   const listElements = stats.map(stat => <StatsListItem key={stat.id} label={stat.label}
@@ -12,3 +13,7 @@ export default function StatList({ stats = [] }) {
     </StatsList>
   );
 }
+Statistics.propTypes = {
+  stats: PropTypes.array,
+};
+
